af3532ad5869cfb26d95f633e9058049897b1c02
verasteguifernando-boop <verasteguifernando@gmail.com>
2026-08-27 03:58:58 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.V6_CONFIG = {
+  SUPABASE_URL: "https://ohzugatyqrgxmtfhmbwu.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_kYkD31AHUzzLZ1YSQeI4bg_Ujh2PucP",
+  EVIDENCE_BUCKET: "core-evidence"
+};
